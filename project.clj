96c74401b74f9061a981be07f8bdc038ebb0d1d1
Luke Gessler <ldg3fa@virginia.edu>
2018-04-16 22:27:53 -0500

    use hickory

--- project.clj
+++ project.clj
@@ -4,7 +4,9 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.8.0"]
+                 [hickory "0.7.1"]
+                 [hiccup "1.0.5"]
                  [clj-tagsoup/clj-tagsoup "0.3.0"]
-                 [hiccup "1.0.5"]]
+                 ]
   :main unicodify-clj.core
   :jvm-opts ["-Xss515m"])
